4b3d3bff49931702c62c2a59637f238eab74d3ff
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-01-02 18:12:24 +0100

    Add try/catch arround server.start

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,15 @@
 int main(int argc, char *argv[]) {
 	(void)argc;
 	(void)argv;
+
 	Server server(6697, "slay", true);
-	server.start();
+
+	try {
+		server.start();
+	} catch (const std::exception &e) {
+		server.log(e.what(), error);
+		return 1;
+	}
+
+	return 0;
 }
