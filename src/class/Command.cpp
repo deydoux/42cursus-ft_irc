57a9f06f68a119ecbc059257fb26ae64f63dc295
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-02-24 13:47:51 +0100

    Add TODO for realname validation in _user command

--- src/class/Command.cpp
+++ src/class/Command.cpp
@@ -71,6 +71,7 @@ void Command::_pass(const args_t &args, Client &client)
 void Command::_user(const args_t &args, Client &client)
 {
 	client.set_username(args[1]);
+	// TODO: check realname
 	client.set_realname(args[4]);
 }
 
