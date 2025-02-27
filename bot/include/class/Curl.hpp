2e3128c8735fd4c0eca236f02450202fd9f5a490
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-02-27 16:38:48 +0100

    Add Curl class implementation and update Makefile for integration

---
+++ bot/include/class/Curl.hpp
@@ -0,0 +1,37 @@
+#ifndef CURL_HPP
+#define CURL_HPP
+
+#include <stdexcept>
+#include <string>
+
+#include <curl/curl.h>
+
+class Curl
+{
+public:
+	Curl();
+	~Curl();
+
+	std::string get(const std::string &url);
+	std::string post(const std::string &url, const std::string &data);
+
+	class Exception : public std::runtime_error
+	{
+	public:
+		Exception(const std::string &message);
+	};
+private:
+	CURL		*_handle;
+	std::string	_buffer;
+
+	void		_reset();
+	CURLcode	_setopt(CURLoption option, const void *data);
+	CURLcode	_setopt_writefunction();
+
+	void		_init(const std::string &url);
+	std::string	_perform();
+
+	static size_t _write_callback(char *ptr, size_t size, size_t nmemb, std::string *userdata);
+};
+
+#endif
--- bot/src/main.cpp
+++ bot/src/main.cpp
@@ -1,29 +1,10 @@
-#include <iostream>
-#include <string>
-#include <curl/curl.h>
+#include "class/Curl.hpp"
 
-static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
-{
-	((std::string*)userp)->append((char*)contents, size * nmemb);
-	return size * nmemb;
-}
+#include <iostream>
 
 int main(void)
 {
-	CURL *curl;
-	std::string buffer;
+	Curl curl;
 
-	curl = curl_easy_init();
-	if (!curl)
-		return 1;
-
-	curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
-	curl_easy_perform(curl);
-	curl_easy_cleanup(curl);
-
-	std::cout << buffer;
-
-	return 0;
+	std::cout << curl.get("https://neofetch.dorian.cool") << std::endl;
 }
