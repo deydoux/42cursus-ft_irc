7cbc1a7b2c534363b37a5edf781035fe8b067ede
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-03-03 14:05:30 +0100

    Add support for Fibonacci sequence in JSON parsing example

--- bot/src/main.cpp
+++ bot/src/main.cpp
@@ -5,7 +5,7 @@
 
 int main(void)
 {
-	JSON::Object obj = JSON::parse<JSON::Object>("   {   \"hello\"   :   \"world\"   ,   \"school\"   :   42   ,   \"truth\"   :   true   ,   \"cat\"   :   { \"height\" : 5 , \"color\" : \"orange\" },\"empty\":null}");
+	JSON::Object obj = JSON::parse<JSON::Object>("   {   \"hello\"   :   \"world\"   ,   \"school\"   :   42   ,   \"truth\"   :   true   ,   \"cat\"   :   { \"height\" : 5 , \"color\" : \"orange\" },\"empty\":null, \"fib\": [0, 1, 1, 2, 3, 5, 8, 13, 21]}   ");
 	JSON::Object cat = obj["cat"].parse<JSON::Object>();
 
 	std::cout << "obj = " << obj << std::endl;
@@ -16,4 +16,5 @@ int main(void)
 	std::cout << "obj.cat.height = " << cat["height"] << std::endl;
 	std::cout << "obj.cat.color = " << cat["color"] << std::endl;
 	std::cout << "obj.empty = " << obj["empty"] << std::endl;
+	std::cout << "obj.fib = " << obj["fib"] << std::endl;
 }
