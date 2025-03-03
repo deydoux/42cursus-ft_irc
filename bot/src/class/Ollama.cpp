ad29148e7072587ee0978aa3c02944f12b2ce6c1
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-03-03 14:48:35 +0100

    Enhance Ollama class to include context in generate method and update main function to demonstrate multiple prompts

--- bot/src/class/Ollama.cpp
+++ bot/src/class/Ollama.cpp
@@ -9,11 +9,19 @@ JSON::Object Ollama::generate(const std::string &prompt, context_t &context)
 	JSON::Object data;
 	data["model"] = _model;
 	data["prompt"] = prompt;
+	data["context"] = context;
 	data["stream"] = false;
 
-	const std::string res = _curl.post(endpoint, data.stringify());
-	return JSON::parse<JSON::Object>(res);
-	(void)context;
+	const std::string raw_res = _curl.post(endpoint, data.stringify());
+	JSON::Object res = JSON::parse<JSON::Object>(raw_res);
+
+	context_t new_context;
+	const JSON::Array res_context = res["context"].parse<JSON::Array>();
+	for (JSON::Array::const_iterator it = res_context.begin(); it != res_context.end(); ++it)
+		new_context.push_back(it->parse<size_t>());
+
+	context = new_context;
+	return res;
 }
 
 Curl Ollama::_curl;
--- bot/src/main.cpp
+++ bot/src/main.cpp
@@ -8,7 +8,18 @@ int main(void)
 	Ollama ollama("llama3.2:1b");
 	Ollama::context_t context;
 
-	JSON::Object res = ollama.generate("Act like Hello Kitty", context);
-	// std::cout << res << std::endl;
-	std::cout << res["response"].parse<std::string>() << std::endl;
+	std::string prompt;
+	JSON::Object res;
+
+	prompt = "Act like Hello Kitty";
+	std::cout << prompt << std::endl << std::endl;
+
+	res = ollama.generate(prompt, context);
+	std::cout << res["response"].parse<std::string>() << std::endl << std::endl;
+
+	prompt = "What you written before?";
+	std::cout << prompt << std::endl << std::endl;
+
+	res = ollama.generate(prompt, context);
+	std::cout << res["response"].parse<std::string>() << std::endl << std::endl;
 }
