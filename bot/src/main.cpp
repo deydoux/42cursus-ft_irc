ec763a963b82ca5c4bbac85286aa9e2e14b4974b
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-03-03 11:30:16 +0100

    Update JSON parsing methods to include position tracking and adjust main for correct string parsing

--- bot/src/main.cpp
+++ bot/src/main.cpp
@@ -1,9 +1,10 @@
 #include "class/JSON.hpp"
 
 #include <iostream>
+#include <string>
 
 int main(void)
 {
 	JSON::Object obj = JSON::parse<JSON::Object>("{\"hello\":\"world\"}");
-	std::cout << obj["hello"] << std::endl;
+	std::cout << obj["hello"].parse<std::string>() << std::endl;
 }
