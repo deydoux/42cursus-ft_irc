526c9bedc1a604d78f6c98a6c1e84e7115a23fbc
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-03-03 13:44:55 +0100

    Enhance JSON parsing to support boolean and null values, and improve error handling

--- bot/src/main.cpp
+++ bot/src/main.cpp
@@ -5,6 +5,13 @@
 
 int main(void)
 {
-	JSON::Object obj = JSON::parse<JSON::Object>("            {             \"hello\"             :             \"world\"         }       ");
-	std::cout << obj["hello"].parse<std::string>() << std::endl;
+	JSON::Object obj = JSON::parse<JSON::Object>("   {   \"hello\"   :   \"world\"   ,   \"school\"   :   42   ,   \"truth\"   :   true   ,   \"cat\"   :   { \"height\" : 5 , \"color\" : \"orange\" }   }   ");
+	JSON::Object cat = obj["cat"].parse<JSON::Object>();
+
+	std::cout << "obj.hello = " << obj["hello"] << std::endl;
+	std::cout << "obj.school = " << obj["school"] << std::endl;
+	std::cout << "obj.truth = " << obj["truth"] << std::endl;
+	std::cout << "obj.cat = " << cat << std::endl;
+	std::cout << "obj.cat.height = " << cat["height"] << std::endl;
+	std::cout << "obj.cat.color = " << cat["color"] << std::endl;
 }
