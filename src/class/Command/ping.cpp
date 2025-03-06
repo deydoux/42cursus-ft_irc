8551f80a186933e5da818918e60429ca33e65587
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-03-06 14:50:52 +0100

    Update ping command handler to allow variable argument count and improve validation

--- src/class/Command/ping.cpp
+++ src/class/Command/ping.cpp
@@ -8,7 +8,7 @@ static void handler(const args_t &args, Client &client, Server &server)
 	if (args.size() == 1)
 		return client.reply(ERR_NOORIGIN, "", "No origin specified");
 
-	if (args.size() == 3 && args[2] != server.get_name())
+	if (args.size() > 2 && args[2] != server.get_name())
 		return client.reply(ERR_NOSUCHSERVER, args[2], "No such server");
 
 	client.cmd_reply(server.get_name(), "PONG", server.get_name(), args[1]);
@@ -17,6 +17,6 @@ static void handler(const args_t &args, Client &client, Server &server)
 const Command::_command_t Command::_ping = {
 	.handler = &handler,
 	.min_args = 0,
-	.max_args = 2,
+	.max_args = -1,
 	.register_mode = registred_only
 };
