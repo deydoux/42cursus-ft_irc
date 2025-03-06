a78f26a08b80a386c9cfa2bd2fa3d4e122b4d858
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-03-06 14:47:27 +0100

    Add ERR_NOORIGIN reply code and update ping command validation logic

--- src/class/Command/ping.cpp
+++ src/class/Command/ping.cpp
@@ -5,12 +5,18 @@
 
 static void handler(const args_t &args, Client &client, Server &server)
 {
+	if (args.size() == 1)
+		return client.reply(ERR_NOORIGIN, "", "No origin specified");
+
+	if (args.size() == 3 && args[2] != server.get_name())
+		return client.reply(ERR_NOSUCHSERVER, args[2], "No such server");
+
 	client.cmd_reply(server.get_name(), "PONG", server.get_name(), args[1]);
 }
 
 const Command::_command_t Command::_ping = {
 	.handler = &handler,
-	.min_args = 1,
-	.max_args = 1,
+	.min_args = 0,
+	.max_args = 2,
 	.register_mode = registred_only
 };
