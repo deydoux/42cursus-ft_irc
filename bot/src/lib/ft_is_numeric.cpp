fbc73843a88870dab670af5ec23f3da4f6bf3c6c
quteriss <quentin.terisse@outlook.com>
2025-03-10 13:38:19 +0100

    Remove ft_is_numeric function and update Makefile dependencies

--- bot/src/lib/ft_is_numeric.cpp
+++
@@ -1,8 +0,0 @@
-#include "lib.hpp"
-
-bool ft_is_numeric(const std::string& s)
-{
-    std::string::const_iterator it = s.begin();
-    while (it != s.end() && std::isdigit(*it)) ++it;
-    return !s.empty() && it == s.end();
-}
