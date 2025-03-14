aba9187eab4777bbb3669f3a070d0f3a32c0a2ce
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-03-14 12:14:39 +0100

    Refactor Server class header: sort by function type then domain

--- include/class/Server.hpp
+++ include/class/Server.hpp
@@ -11,62 +11,61 @@ class Client;
 class Server
 {
 public:
-	// Types
+// Types
 	typedef uint16_t	port_t;
 
-	// Static variables
+// Static variables
 	static bool	stop;
 
-
 private:
-	// Types
+// Types
 	typedef std::vector<struct pollfd>	_pollfds_t;
 
-	// Static variables
+// Static variables
 	static const port_t	_default_port = 6697;
 	static const bool	_default_verbose = true;
 
-
 public:
-	// Static functions
+// Static functions
 	static Server	parse_args(int argc, char *argv[]);
 
-
+// Constructors/destructors
 	Server(const std::string &name, port_t port, const std::string &password, const std::string &motd, const std::string &motd_file, bool verbose);
 	~Server();
 
+// Member functions
 	void	log(const std::string &message, const log_level level = info) const;
 	void	start();
 
+// Getters
+	// Server
 	bool							check_password(const std::string &password) const;
 	const bool						&is_verbose() const;
 	const std::string				&get_name() const;
 	const std::string				&get_start_time() const;
 	const std::vector<std::string>	&get_motd_lines() const;
-
 	// Stats
-	void	register_client();
-
 	size_t	get_channels_count() const;
 	size_t	get_clients_count() const;
 	size_t	get_connections() const;
 	size_t	get_max_clients() const;
 	size_t	get_max_connections() const;
-
 	// Channels
-	void	add_channel(Channel &new_channel);
-	void	delete_channel(const std::string &name);
-
 	Channel		*get_channel(const std::string &channel_name) const;
 	channels_t	get_channels() const;
-
 	// Clients
 	Client		*get_client(const std::string &nickname) const;
 	clients_t	get_clients(const std::string &mask) const;
 
+// Setters
+	// Stats
+	void	register_client();
+	// Channels
+	void	add_channel(Channel &new_channel);
+	void	delete_channel(const std::string &name);
 
 private:
-	// Static functions
+// Static functions
 	static port_t			_parse_port(const std::string &port_str);
 	static sockaddr_in		_init_address(port_t port);
 	static std::string		_get_next_arg(int &i, int argc, char *argv[]);
@@ -74,7 +73,7 @@ private:
 	static void				_print_usage(int status = 1);
 	static void				_signal_handler(int sig);
 
-
+// Variables
 	const bool			_verbose;
 	const port_t		_port;
 	const sockaddr_in	_address;
@@ -87,7 +86,17 @@ private:
 	int							_socket;
 	std::string					_start_time;
 	std::vector<std::string>	_motd_lines;
+	// Stats
+	size_t	_connections;
+	size_t	_max_connections;
+	size_t	_max_registered_clients;
+	size_t	_registered_clients_count;
+	// Channels
+	channels_t	_channels;
+	// Clients
+	clients_t	_clients;
 
+// Member functions
 	void	_accept();
 	void	_bind();
 	void	_down();
@@ -100,19 +109,8 @@ private:
 	void	_set_signal_handler();
 	void	_set_start_time();
 
-
-	// Stats
-	size_t	_connections;
-	size_t	_max_connections;
-	size_t	_max_registered_clients;
-	size_t	_registered_clients_count;
-
-	// Channels
-	channels_t	_channels;
-
+// Setters
 	// Clients
-	clients_t	_clients;
-
 	void	_disconnect_client(int fd);
 };
 
