70d109afe523f8dcf9080215df56afd25d68e70e
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-03-14 11:19:29 +0100

    Refactor Server class: reorganize member variables and methods for improved readability and maintainability

--- include/class/Server.hpp
+++ include/class/Server.hpp
@@ -10,67 +10,63 @@ class Client;
 
 class Server
 {
+// Types
 public:
 	typedef uint16_t	port_t;
 
+private:
+	typedef std::vector<struct pollfd>	_pollfds_t;
+
+
+// Static variables
+public:
+	static bool	stop;
+
+private:
+	static const port_t	_default_port = 6697;
+	static const bool	_default_verbose = true;
+
+
+// Static functions
+public:
+	static Server	parse_args(int argc, char *argv[]);
+
+private:
+	static port_t			_parse_port(const std::string &port_str);
+	static sockaddr_in		_init_address(port_t port);
+	static std::string		_get_next_arg(int &i, int argc, char *argv[]);
+	static struct pollfd	_init_pollfd(int fd);
+	static void				_print_usage(int status = 1);
+	static void				_signal_handler(int sig);
+
+
+// Server
+public:
 	Server(const std::string &name, port_t port, const std::string &password, const std::string &motd, const std::string &motd_file, bool verbose);
 	~Server();
 
 	void	log(const std::string &message, const log_level level = info) const;
 	void	start();
 
-	bool	check_password(const std::string &password) const;
-	void	register_client();
-
-	const std::string	&get_name() const;
-	const bool			&is_verbose() const;
-	const std::string	&get_start_time() const;
-
-	void	add_channel(Channel &new_channel);
-	void	delete_channel(std::string name);
-
+	bool							check_password(const std::string &password) const;
+	const bool						&is_verbose() const;
+	const std::string				&get_name() const;
+	const std::string				&get_start_time() const;
 	const std::vector<std::string>	&get_motd_lines() const;
 
-	Client		*get_client(const std::string &nickname) const;
-	clients_t	get_clients(const std::string &mask);
-	
-	Channel		*get_channel(const std::string &channel_name);
-	channels_t	get_channels( void );
-
-	size_t	get_connections() const;
-	size_t	get_max_connections() const;
-	size_t	get_clients_count() const;
-	size_t	get_max_clients() const;
-	size_t	get_channels_count() const;
-
-	static bool	stop;
-
-	static Server	parse_args(int argc, char *argv[]);
-
 private:
-	typedef std::vector<struct pollfd>	_pollfds_t;
-
-	const std::string	_name;
-	const port_t		_port;
-	const std::string	_password;
-	const std::string	_motd;
-	const std::string	_motd_file;
 	const bool			_verbose;
+	const port_t		_port;
 	const sockaddr_in	_address;
+	const std::string	_motd_file;
+	const std::string	_motd;
+	const std::string	_name;
+	const std::string	_password;
 
-	int			_socket;
-	_pollfds_t	_pollfds;
-
+	_pollfds_t					_pollfds;
+	int							_socket;
 	std::string					_start_time;
 	std::vector<std::string>	_motd_lines;
-
-	size_t	_connections;
-	size_t	_max_connections;
-	size_t	_max_registered_clients;
-	size_t	_registered_clients_count;
-
-	clients_t	_clients;
-	channels_t	_channels;
 
 	void	_init_motd();
 	void	_set_start_time();
@@ -84,17 +80,45 @@ private:
 	void	_read();
 	void	_down();
 
+
+// Server stats
+public:
+	void	register_client();
+
+	size_t	get_channels_count() const;
+	size_t	get_clients_count() const;
+	size_t	get_connections() const;
+	size_t	get_max_clients() const;
+	size_t	get_max_connections() const;
+
+private:
+	size_t	_connections;
+	size_t	_max_connections;
+	size_t	_max_registered_clients;
+	size_t	_registered_clients_count;
+
+
+// Channels
+public:
+	void	add_channel(Channel &new_channel);
+	void	delete_channel(const std::string &name);
+
+	Channel		*get_channel(const std::string &channel_name) const;
+	channels_t	get_channels() const;
+
+private:
+	channels_t	_channels;
+
+
+// Clients
+public:
+	Client		*get_client(const std::string &nickname) const;
+	clients_t	get_clients(const std::string &mask) const;
+
+private:
+	clients_t	_clients;
+
 	void	_disconnect_client(int fd);
-
-	static const port_t	_default_port = 6697;
-	static const bool	_default_verbose = true;
-
-	static std::string		_get_next_arg(int &i, int argc, char *argv[]);
-	static port_t			_parse_port(const std::string &port_str);
-	static void				_print_usage(int status = 1);
-	static sockaddr_in		_init_address(port_t port);
-	static struct pollfd	_init_pollfd(int fd);
-	static void				_signal_handler(int sig);
 };
 
 #endif // SERVER_HPP
