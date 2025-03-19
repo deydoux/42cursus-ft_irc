70104ab22421a9083e94d87c99896409720411ec
quteriss <quentin.terisse@outlook.com>
2025-03-19 13:38:04 +0100

    reorganise TriviaGame methods and header

--- bot/include/class/TriviaGame.hpp
+++ bot/include/class/TriviaGame.hpp
@@ -3,6 +3,8 @@
 
 #include "lib.hpp"
 
+#include <unistd.h>
+
 #include <algorithm>
 #include <ctime>
 #include <cstdlib>
@@ -14,6 +16,7 @@ class IRC;
 class TriviaGame
 {
 public:
+	// -- STRUCTS
 	typedef	std::vector<std::string> phrases_t;
 	
 	typedef	struct question_s {
@@ -36,36 +39,32 @@ public:
 	} player_t;
 	typedef	std::vector<player_t> players_t;
 
+	// -- CONSTRUCTOR + DESTRUCTOR
 	TriviaGame(IRC &irc_client, std::string channel_name, std::vector<std::string> players, bool verbose = true);
 	~TriviaGame();
 
-	void	log(const std::string &message, const log_level level = info) const;
+	// -- GETTERS + SETTERS
+	bool		is_waiting_for_answers( void );
+	bool		is_waiting_before_start( void );
+	std::string	get_channel( void );
 
-	bool	is_waiting_for_answers( void );
+	void		add_player(const std::string &client_nickname);
+	void		remove_player(const std::string &client_nickname);
 
-	void	send(const std::string &message, int send_delay = 0);
-	std::string create_reply(const std::string &message);
-	std::string create_empty_reply( void );
-	void	add_player(const std::string &client_nickname);
-	void	remove_player(const std::string &client_nickname);
-	void	greet_players( void );
+	// -- PUBLIC METHODS
 	void	tick( void );
-	void	ask_trivia_question( void );
-	void	store_answer(const std::string &answer, const std::string &client_nickname);
-	void	show_round_results( void );
+	void	greet_players( void );
 	void	show_final_results( void );
 	void	mark_user_as_ready(const std::string &client_nickname);
-	bool	is_waiting_before_start( void );
+	void	store_answer(const std::string &answer, const std::string &client_nickname);
 
-	void	initialize_round( void );
+	void	log(const std::string &message, const log_level level = info) const;
 
-	std::string get_channel( void);
-
+	// -- STATIC METHODS
 	static void			initialize_phrases( void );
 	static std::string	pick_randomly(const phrases_t phrases);
 
-	player_t	create_player(const std::string &nick);
-
+	// -- I18N
 	static std::string	greetings_header;
 	static phrases_t	greetings_subheader;
 	static std::string	game_rules;
@@ -81,27 +80,38 @@ public:
 	static phrases_t	farewells;
 
 private:
+	// -- PRIVATE ATTRIBUTES
 	IRC					&_irc_client;
-	players_t			_players;
 	size_t				_round_counter;
+	players_t			_players;
 	questions_t			_questions;
 	std::map<char, std::string> _choices;
 	std::time_t			_asked_at;
 	bool				_waiting_before_start;
 	bool				_waiting_for_answers;
 	bool				_first_player_answered;
-
 	bool				_rang_timer;
 
+	// -- PRIVATE CONSTANTS
 	const std::string	_channel;
 	const bool			_verbose;
 
+	// -- PRIVATE METHODS
 	questions_t	_fetch_questions();
 	void		_start_game( void );
+	player_t	_create_player(const std::string &nick);
 	player_t	*_get_player(const std::string &nickname);
+	std::string	_create_reply(const std::string &message);
+	std::string	_create_empty_reply( void );
+	void		_send(const std::string &message, int send_delay = 0);
+	void		_initialize_round( void );
+	void		_ask_trivia_question( void );
+	void		_show_round_results( void );
 
-	static bool	compare_by_total_score(const player_t &p1, const player_t &p2);
+	// -- PRIVATE STATIC METHODS
+	static bool	_compare_by_total_score(const player_t &p1, const player_t &p2);
 
+	// -- PRIVATE STATIC CONSTANTS + ATTRIBUTES
 	static const int	_nb_rounds = 10;
 	static const int	_round_duration_sec = 30;
 	static const int	_points = 10;
