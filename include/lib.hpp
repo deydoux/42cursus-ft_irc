b789e946ca8906fa64f5257667e4f57847919316
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-03-20 18:13:33 +0100

    Refactor lib.hpp: reorganize typedefs and function declarations for improved clarity and consistency

--- include/lib.hpp
+++ include/lib.hpp
@@ -15,20 +15,23 @@
 class Channel;
 class Client;
 
-typedef std::map<int, Client *>				clients_t; //	{client fd, client}
-typedef std::map<std::string, Channel *>	channels_t; //	{channel name, channel}
+// Types
+typedef enum {
+	debug,
+	info,
+	warning,
+	error
+}	log_level;
+typedef std::map<int, Client *>				clients_t; // clients_t[fd] = Client *
+typedef std::map<std::string, Channel *>	channels_t; // channels_t[name] = Channel *
 typedef std::vector<std::string>			args_t;
 
-typedef enum {debug, info, warning, error}	log_level;
-
-void	log(const std::string &context, const std::string &message, const log_level level = info);
-
-std::string		to_lower(const std::string &str);
-bool			match_mask(const std::string& mask, const std::string& user_string);
-std::vector<std::string> ft_split(const std::string &str, char separator);
-std::string		unix_timestamp( void );
-
+bool						match_mask(const std::string& mask, const std::string& user_string);
+std::string					to_lower(const std::string &str);
+std::string					unix_timestamp();
+std::vector<std::string>	ft_split(const std::string &str, char separator);
+void						log(const std::string &context, const std::string &message, const log_level level = info);
 template <typename T>
-std::string to_string(T n);
+std::string					to_string(T n);
 
 #endif // LIB_HPP
